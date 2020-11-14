d41c3bcf65500a87b6090ca151db63957fc3505c
khhc <70731689+khhc@users.noreply.github.com>
2020-11-13 22:13:09 -1000

    First half draft of Landing Page

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -1,24 +1,68 @@
 import React from 'react';
-import { Grid, Image } from 'semantic-ui-react';
+import { Grid, Header, Segment, List } from 'semantic-ui-react';
 
 /** A simple static component to render some text for the landing page. */
 class Landing extends React.Component {
   render() {
     return (
-        <Grid id='landing-page' verticalAlign='middle' textAlign='center' container>
+        <div className='bg-image'>
+          <Grid container centered stackable>
+            <Grid.Row>
+              <Header as='h1' size='huge' inverted color='green'>
+                Manoa Tunes
+                <Header.Subheader>
+                  A Hub for People to Connect through the Power of Music
+                </Header.Subheader>
+              </Header>
+            </Grid.Row>
 
-          <Grid.Column width={4}>
-            <Image size='small' circular src="/images/meteor-logo.png"/>
-          </Grid.Column>
+            <Grid.Row columns={2}>
+              <Grid.Column textAlign='left'>
+                <Header as='h3' attached='top'>
+                  Upcoming Jam Meeting
+                </Header>
+                <Segment attached>
+                  Jam session with X and Y
+                  Meeting on mm/dd/yyyy
+                </Segment>
+              </Grid.Column>
+              <Grid.Column textAlign='right'>
+                <Header as='h3' attached='top'>
+                  Featured Track
+                </Header>
+                <Segment attached>
+                  We can either add an image here or just have a link and short description of a song.
+                </Segment>
+              </Grid.Column>
+            </Grid.Row>
 
-          <Grid.Column width={8}>
-            <h1>Welcome to this template</h1>
-            <p>Now get to work and modify this app!</p>
-          </Grid.Column>
-
+            <Grid.Row columns={2}>
+              <Grid.Column textAlign='left'>
+                <Header as='h3' attached='top'>
+                  Features
+                </Header>
+                <Segment attached>
+                  <List bulleted>
+                    <List.Item>Keyword Interest Searching</List.Item>
+                    <List.Item>Planning In-Person Events</List.Item>
+                    <List.Item>Clickable Links to Artist's Music</List.Item>
+                  </List>
+                </Segment>
+              </Grid.Column>
+              <Grid.Column textAlign='right'>
+                <Header as='h3' attached='top'>
+                  Frequently Asked Questions
+                </Header>
+                <Segment attached>
+                  What is the meaning of life?
+                  Not too sure, but I keep hearing this 42 business.
+                </Segment>
+              </Grid.Column>
+            </Grid.Row>
         </Grid>
-    );
+  </div>
+  );
   }
-}
+  }
 
-export default Landing;
+  export default Landing;
