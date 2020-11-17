82c1c9632765957dcd4c2f2608458c3e2e39780b
Michael Hui <70671003+huimichael@users.noreply.github.com>
2020-11-17 11:15:31 -1000

    Footer

--- app/imports/ui/components/Footer.jsx
+++ app/imports/ui/components/Footer.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Grid, GridColumn, Header, Icon, Item, List } from 'semantic-ui-react';
+import Image from 'semantic-ui-react/dist/commonjs/elements/Image';
 
 /** The Footer appears at the bottom of every page. Rendered by the App Layout component. */
 class Footer extends React.Component {
@@ -8,13 +9,14 @@ class Footer extends React.Component {
         <div className="footer">
           <footer>
             <div className="ui container">
-              <Grid columns={2}>
+              <Grid columns={3}>
                 <GridColumn className="inverted">
                   <hr />
                   <Header className="inverted" textAlign="left">Navigation</Header>
                   <List inverted>
-                    <List.Item href='http://localhost:3000/#/'>Home</List.Item>
-                    <List.Item href='https://manoa-tunes.github.io/'>About Us</List.Item>
+                    <List.Item href='/' className="text">Home</List.Item>
+                    <List.Item href='https://manoa-tunes.github.io/' className="text">About Us</List.Item>
+                    <List.Item href='/signup#/signup' className="text">Sign Up</List.Item>
                   </List>
                 </GridColumn>
 
@@ -23,27 +25,32 @@ class Footer extends React.Component {
                   <Header className="inverted" textAlign="left">Contact Information</Header>
                       <List inverted>
                         <List.Item className="text">
+                          Our Github
+                          <a href="https://github.com/manoa-tunes/manoa-tunes"> <Icon name="github" size="large"/></a>
+                        </List.Item>
+                        <List.Item className="text">
                           Cheolhoon Choi
-                          <a href="https://cheolhoon.github.io/"> <Icon name="github" size="large"/></a>
                           <a href="mailto: choi4879@hawaii.edu"> <Icon name="mail" size="large"/> </a>
                         </List.Item>
                         <List.Item className="text">
                           Henry Cheung
-                          <a href="https://khhc.github.io/"> <Icon name="github" size="large"/> </a>
                           <a href="mailto: khhc@hawaii.edu"> <Icon name="mail" size="large"/> </a>
                         </List.Item>
                         <List.Item className="text">
                           Michael Hui
-                          <a href="https://huimichael.github.io/"> <Icon name="github" size="large"/> </a>
                           <a href="mailto: huim@hawaii.edu"> <Icon name="mail" size="large"/> </a>
                         </List.Item>
                         <List.Item className="text">
                           Timothy Huo
-                          <a href="https://timothyhuo1.github.io/"> <Icon name="github" size="large"/> </a>
                           <a href="mailto: thuo@hawaii.edu"> <Icon name="mail" size="large"/> </a>
                         </List.Item>
                       </List>
+                </GridColumn>
 
+                <GridColumn>
+                  <hr/>
+                  <Image size="tiny"
+                      src="https://anad.org/wp-content/uploads/2017/09/university-of-hawaii-manoa.png"/>
                 </GridColumn>
               </Grid>
             </div>
