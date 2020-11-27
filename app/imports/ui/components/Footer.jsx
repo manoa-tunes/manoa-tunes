bf7c6108dfe66cc008a1968e1204f848dfdd919c
Michael Hui <70671003+huimichael@users.noreply.github.com>
2020-11-26 20:32:43 -1000

    Updated Footer and Signout Page

--- app/imports/ui/components/Footer.jsx
+++ app/imports/ui/components/Footer.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Grid, GridColumn, Header, Icon, List } from 'semantic-ui-react';
+import { Grid, GridColumn, Header, Icon, Image, List } from 'semantic-ui-react';
 
 /** The Footer appears at the bottom of every page. Rendered by the App Layout component. */
 class Footer extends React.Component {
@@ -8,52 +8,28 @@ class Footer extends React.Component {
         <div className="footer">
           <footer>
             <div className="ui container">
-              <Grid columns={3}>
+              <Grid columns={2}>
                 <GridColumn className="inverted">
                   <hr />
                   <Header className="inverted" textAlign="left">Navigation</Header>
                   <List inverted>
-                    <List.Item href='/' className="text">Home</List.Item>
+                    <List.Item href='https://manoatunes.xyz/' className="text">Home</List.Item>
+                    <List.Item href='https://github.com/manoa-tunes/manoa-tunes' className='text'>Our Github</List.Item>
                     <List.Item href='https://manoa-tunes.github.io/' className="text">About Us</List.Item>
+                    <List.Item href='https://www.meteor.com/install' className="text">Meteor Installation</List.Item>
                     <List.Item href='/signup#/signup' className="text">Sign Up</List.Item>
                   </List>
                 </GridColumn>
 
                 <GridColumn>
                   <hr />
-                  <Header className="inverted" textAlign="left">Contact Information</Header>
-                      <List inverted>
-                        <List.Item className="text">
-                          Our Github
-                          <a href="https://github.com/manoa-tunes/manoa-tunes"> <Icon name="github" size="large"/></a>
-                        </List.Item>
-                        <List.Item className="text">
-                          Cheolhoon Choi
-                          <a href="mailto: choi4879@hawaii.edu"> <Icon name="mail" size="large"/> </a>
-                        </List.Item>
-                        <List.Item className="text">
-                          Henry Cheung
-                          <a href="mailto: khhc@hawaii.edu"> <Icon name="mail" size="large"/> </a>
-                        </List.Item>
-                        <List.Item className="text">
-                          Michael Hui
-                          <a href="mailto: huim@hawaii.edu"> <Icon name="mail" size="large"/> </a>
-                        </List.Item>
-                        <List.Item className="text">
-                          Timothy Huo
-                          <a href="mailto: thuo@hawaii.edu"> <Icon name="mail" size="large"/> </a>
-                        </List.Item>
-                      </List>
-                </GridColumn>
-
-                <GridColumn>
-                  <hr />
+                  <Image size='tiny' src="/images/logo.png"/>
                   <List inverted>
                     <List.Item className="text">Manoa-Tunes Team</List.Item>
                     <List.Item className="text">ICS 314 Project</List.Item>
                     <List.Item className="text">University of Hawaii</List.Item>
                     <List.Item className="text">Honolulu, HI 96822</List.Item>
-                    <List.Item href='https://manoa-tunes.github.io/' className="text">Home Page</List.Item>
+                    <List.Item className="text"><Icon name='copyright'/> Manoa-Tunes 2020   </List.Item>
                   </List>
                 </GridColumn>
               </Grid>
