0cb936e16db28a0ddcad495f78bdb7c61bc83e06
Timt <61216890+TimothyH808@users.noreply.github.com>
2020-11-28 09:36:40 -1000

    testcafe

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -6,7 +6,7 @@ class Landing extends React.Component {
   render() {
     return (
         <div className='bg-image'>
-          <Grid container centered stackable>
+          <Grid container centered stackable id='landing-page'>
             <Grid.Row>
               <Header as='h1' size='huge' inverted color='green'>
                 Manoa Tunes
