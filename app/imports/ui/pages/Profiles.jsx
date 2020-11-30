5ac78cca2c889ac94a7743ced284f8f97ff4ac9a
Cheolhoon Choi <54978134+cheolhoon@users.noreply.github.com>
2020-11-29 21:35:34 -1000

    display profiles 1

--- app/imports/ui/pages/Profiles.jsx
+++ app/imports/ui/pages/Profiles.jsx
@@ -6,6 +6,7 @@ import PropTypes from 'prop-types';
 import { _ } from 'meteor/underscore';
 import { Profiles } from '../../api/profiles/Profiles';
 import { ProfilesInterests } from '../../api/profiles/ProfilesInterests';
+import { ProfilesInstruments } from '../../api/profiles/ProfilesInstruments';
 import { ProfilesJams } from '../../api/profiles/ProfilesJams';
 import { Jams } from '../../api/jams/Jams';
 import { ProfileCard } from '../components/ProfileCard';
@@ -14,10 +15,11 @@ import { ProfileCard } from '../components/ProfileCard';
 function getProfileData(email) {
   const data = Profiles.collection.findOne({ email });
   const interests = _.pluck(ProfilesInterests.collection.find({ profile: email }).fetch(), 'interest');
+  const instruments = _.pluck(ProfilesInstruments.collection.find({ profile: email }).fetch(), 'instrument');
   const jams = _.pluck(ProfilesJams.collection.find({ profile: email }).fetch(), 'jam');
   const jamPictures = jams.map(jam => Jams.collection.findOne({ name: jam }).picture);
   // console.log(_.extend({ }, data, { interests, jams: jamPictures }));
-  return _.extend({}, data, { interests, jams: jamPictures });
+  return _.extend({}, data, { interests, instruments, jams: jamPictures });
 }
 
 /** Renders the Profile Collection as a set of Cards. */
@@ -51,9 +53,10 @@ export default withTracker(() => {
   // Ensure that minimongo is populated with all collections prior to running render().
   const sub1 = Meteor.subscribe(Profiles.userPublicationName);
   const sub2 = Meteor.subscribe(ProfilesInterests.userPublicationName);
-  const sub3 = Meteor.subscribe(ProfilesJams.userPublicationName);
-  const sub4 = Meteor.subscribe(Jams.userPublicationName);
+  const sub3 = Meteor.subscribe(ProfilesInstruments.userPublicationName);
+  const sub4 = Meteor.subscribe(ProfilesJams.userPublicationName);
+  const sub5 = Meteor.subscribe(Jams.userPublicationName);
   return {
-    ready: sub1.ready() && sub2.ready() && sub3.ready() && sub4.ready(),
+    ready: sub1.ready() && sub2.ready() && sub3.ready() && sub4.ready() && sub5.ready(),
   };
 })(ProfilesPage);
