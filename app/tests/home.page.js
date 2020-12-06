d44321d58a9c668df7a8fabd5f579c093d5af9e4
Cheolhoon Choi <54978134+cheolhoon@users.noreply.github.com>
2020-12-06 13:47:48 -1000

    home1

---
+++ app/tests/home.page.js
@@ -0,0 +1,52 @@
+import { Selector } from 'testcafe';
+
+class HomePage {
+  constructor() {
+    this.pageId = '#home-page';
+    this.pageSelector = Selector(this.pageId);
+  }
+
+  /** Checks that this page is currently displayed. */
+  async isDisplayed(testController) {
+    await testController.expect(this.pageSelector.exists).ok();
+  }
+
+  /** Sets the name field to a new value, then checks that the update succeeded. */
+  async setName(testController, name) {
+    // Delete text from name field.
+    await testController.selectText('#name').pressKey('delete');
+    // Type in new name.
+    await testController.typeText('#name', name);
+    // Submit it.
+    await testController.click('#home-page-submit');
+    // Click the OK button on the Sweet Alert.
+    await testController.click(Selector('.swal-button--confirm'));
+    // Check that the field is updated.
+    await testController.expect(Selector('#name').value).eql(name);
+  }
+
+  /** Checks this page is displayed, then changes name field, checks update succeeded, then restores value. */
+  // Should be able to use set name without generating warnings with first release after 1.9.4. (Fixed in pull/5584).
+  async updateProfile(testController, name) {
+    const newName = 'New Name';
+    await this.isDisplayed(testController);
+    // Delete text from name field.
+    await testController.selectText('#name').pressKey('delete');
+    // Type in new name.
+    await testController.typeText('#name', newName);
+    // Submit it.
+    await testController.click('#home-page-submit');
+    // Click the OK button on the Sweet Alert.
+    await testController.click(Selector('.swal-button--confirm'));
+    // Check that the field is updated.
+    await testController.expect(Selector('#name').value).eql(newName);
+    // Now restore original value.
+    await testController.selectText('#name').pressKey('delete');
+    await testController.typeText('#name', name);
+    await testController.click('#home-page-submit');
+    await testController.click(Selector('.swal-button--confirm'));
+    await testController.expect(Selector('#name').value).eql(name);
+  }
+}
+
+export const homePage = new HomePage();
--- app/tests/tests.testcafe.js
+++ app/tests/tests.testcafe.js
@@ -2,6 +2,7 @@ import { landingPage } from './landing.page';
 import { signinPage } from './signin.page';
 import { signoutPage } from './signout.page';
 import { navBar } from './navbar.component';
+import { homePage } from './home.page';
 
 /* global fixture:false, test:false */
 
@@ -21,4 +22,13 @@ test('Test that signin and signout work', async (testController) => {
   await navBar.isLoggedIn(testController, credentials.username);
   await navBar.logout(testController);
   await signoutPage.isDisplayed(testController);
+});
+
+test('Test that home page display and profile modification works', async (testController) => {
+  await navBar.ensureLogout(testController);
+  await navBar.gotoSigninPage(testController);
+  await signinPage.signin(testController, credentials.username, credentials.password);
+  await homePage.isDisplayed(testController);
+  await homePage.updateProfile(testController, credentials.firstName);
+  await navBar.ensureLogout(testController);
 });
